03aef02975c3721c57c7a9324d2bbeabf235909f
sho.kiritani <kiritani5038@icloud.com>
2021-03-14 13:35:13 +0000

    correct user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
     before_action :set_task , only:[:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:destroy, :update, :show]
+  
     
     def index
         @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(25)
@@ -53,6 +55,14 @@ class TasksController < ApplicationController
     
     def set_task
         @task = current_user.tasks.find(params[:id])
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            flash[:danger] = "他のユーザーのタスクは変更できません。"
+            redirect_to root_url
+        end
     end
     
 end
